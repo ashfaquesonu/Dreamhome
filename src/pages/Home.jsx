b60103c478e7540ededb81cb864dbf9dcd40ad72
Muhammed Ashfaque M <ashfaque678583@gmail.com>
2023-10-10 10:59:45 +0530

    builders

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from '../Component/Navbar';
+import Builder from '../Component/Builders/Builder';
+
+
+function Home() {
+
+    
+    
+
+  return (
+    <div>
+      <Navbar/>
+      <Builder/>
+    
+
+    </div>
+    
+
+
+);
+}
+    
+  
+
+
+export default Home
+
